ed0123dc78845cabc8a6cdec643aaf16c162536d
nitishchetak <91657439+nitishchetak@users.noreply.github.com>
2021-11-01 19:37:55 +0530

    Add files via upload

---
+++ Q1 link1.js
@@ -0,0 +1,3 @@
+var x=3;
+console.log(x);
+console.log("js file is linked by using script in head");
---
+++ Q1 link2.js
@@ -0,0 +1,3 @@
+var x=8;
+console.log(x);
+console.log("js file is linked by using sript in body");
